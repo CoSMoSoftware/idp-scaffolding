c6743647d570628c3493647a385b1af9cd4abdcf
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2018-03-15 11:06:41 +0100

    Add config and register idp function

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const IdentityProvider = require("./lib/IdentityProvider");
 
 //Load google idendity provider config
-const config = require("./credentials.json");
+const config = require("./config.json");
 
 //Create an idp instance
-const idp = new IdentityProvider({});
+const idp = new IdentityProvider(config);
 
-//Register identity provider on global RTCIdentityProviderRegistrar 
-global.rtcIdentityProvider.register(idp);
+//Export module registration function
+module.exports = function() {
+	//Register identity provider on global RTCIdentityProviderRegistrar 
+	global.rtcIdentityProvider.register(idp);
+};
